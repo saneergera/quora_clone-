28f3640dcdb961dbae53014c4aca29b0949067f4
saneergera <saneergera@gmail.com>
2018-11-06 11:47:11 +0530

    upload the files

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Hello" });
-});
-
 require("./routes/authentication.js")(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
